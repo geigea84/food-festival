1b0346457d49116d4832d8bf0e471495f9f191bb
geigea84 <geigea84@gmail.com>
2021-01-19 22:54:25 -0600

    finished 19.4

---
+++ service-worker.js
@@ -0,0 +1,71 @@
+//19.4.5
+const APP_PREFIX = "FoodFest-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./events.html",
+    "./tickets.html",
+    "./schedule.html",
+    "./assets/css/style.css",
+    "./assets/css/bootstrap.css",
+    "./assets/css/tickets.css",
+    "./dist/app.bundle.js",
+    "./dist/events.bundle.js",
+    "./dist/tickets.bundle.js",
+    "./dist/schedule.bundle.js"
+];
+
+/* Look at the following listener. Why don't we use 
+window.addEventListener instead of self? Well, service 
+workers run before the window object has even been created. 
+So instead we use the self keyword to instantiate listeners 
+on the service worker. The context of self here refers to 
+the service worker object. */
+self.addEventListener("install", function(e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            console.log("installing cache: " + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+//19.4.6
+self.addEventListener("activate", function(e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            let cacheKeepList = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            })
+            cacheKeepList.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function(key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log("deleting cache: " + keyList[i]);
+                    return caches.delete(keyList[i]);
+                }
+            }));
+        })
+    )
+})
+
+//19.4.7
+self.addEventListener("fetch", function(e) {
+    console.log("fetch request: " + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function(request) {
+            if (request) { //if cache is available, respond with cache 
+                console.log("responding with cache: " + e.request.url)
+                return request
+            }
+            else { //if there are no cache, try fetching request
+                console.log("file is not cached, fetching: " + e.request.url)
+                return fetch(e.request)
+            }
+            
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request)
+        })
+    )
+})
